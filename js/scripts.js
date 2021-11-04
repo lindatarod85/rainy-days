db31f8f6b7cded7d70ca31fdc75bf7174fc13a75
lindatarod85 <lindatarod@gmail.com>
2021-11-04 12:31:56 +0100

    Added hotjar tracking code

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.querySelector(".add").onclick = function () {
+  document.querySelector(".checkout").innerHTML = "Clicked";
+  document.querySelector(".checkout").style.backgroundColor = "red";
+};
