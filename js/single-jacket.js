43954dac0fb1df4237b760dd30af0739e4e30d03
lindatarod85 <lindatarod@gmail.com>
2022-02-10 16:42:37 +0100

    Styling

--- js/single-jacket.js
+++ js/single-jacket.js
@@ -31,11 +31,46 @@ fetchJacket();
 
 function createHTML(results){
     singleJacketContainer.innerHTML +=`<div class="jacket-container">
+    
     <h1>${results.name}</h1>
-   
+    <div class="jacket-image-container">
 
-    <p><span>Price ${results.price}</span></p>
+    <img class="main-image" src="${results.images[0].src}" alt="${results.name}">
+   
+    <div class="thumbnails">
+    <img src="${results.images[0].src}" onclick="change(this.src)" alt="${results.name}">
+    <img src="${results.images[1].src}" onclick="change(this.src)" alt="${results.name}">
+    <img src="${results.images[2].src}" onclick="change(this.src)" alt="${results.name}">
+   <img src="${results.images[3].src}" onclick="change(this.src)" alt="${results.name}">
+   </div>
+    </div>
+
+
+ <div class="jacket-info">  
+<p class="jacket-categories"><span>Categories: </span>${results.categories[1].name}, ${results.categories[0].name}</p>
+
+    <p class="short-description">${results.short_description}</p>
+
+    <label for="select-options">${results.add_to_cart.text}</label>
+          <select name="select-options" id="select-options ">
+            <option value="S">${results.variations[0].attributes[0].value}</option>
+            <option value="M">${results.variations[1].attributes[0].value}</option>
+            <option value="L">${results.variations[2].attributes[0].value}</option>
+          </select>
+   
+    <p class="jacket-price"><span>Price:</span> ${results.prices.price},- ${results.prices.currency_code}</p>
 
     
-    </div>`
+
+    
+ </div>
+ 
+ </div>
+ `
 }
+
+/*Image thumbnail gallery function */
+
+const change = (src) => {
+    document.querySelector(".main-image").src = src;
+  };
